55adbd292e50d6437afa5a1a057a87e4472bf811
Harry Gallagher <harrygallagher4@gmail.com>
2019-06-13 18:30:57 -0500

    Add base16-shell

--- zshrc/init.zsh
+++ zshrc/init.zsh
@@ -10,3 +10,8 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 source $ZSH/oh-my-zsh.sh
 source ~/.purepower
 
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        eval "$("$BASE16_SHELL/profile_helper.sh")"
